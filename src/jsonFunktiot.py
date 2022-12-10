bfdc296fb6f1978aab7bad4d4021b4b5b20db2b8
Anna Suuronen <annasuuronen@icloud.com>
2022-12-10 22:25:45 +0200

    jsonFunktiot-tiedostoon lisätty kommentit

--- src/jsonFunktiot.py
+++ src/jsonFunktiot.py
@@ -1,11 +1,22 @@
 import json
 
 def avaaJson():
+    """Avaa tiedoston data.json, joka sisältää kappaleiden säveliä ja nuotteja,
+    ja muuttaa sen sisällön pythonin ymmärtämäksi dictionaryksi
+
+    Returns:
+        data.json-tiedoston sisältämä dictionary sävelistä ja nuoteista pythonin ymmärtämässä muodossa 
+    """
     with open('data.json', 'r') as data:
         arvo = json.loads(data.read())
         return arvo
 
 def tallennaJson(tallennettava):
+    """Tallentaa data.json-tiedostoon annetun dictionaryn
+
+    Args:
+        tallennettava: python-muodossa oleva dictionary, joka halutaan talettaa data.json-tiedoston sisällöksi
+    """
     with open('data.json', 'w') as data:
         arvo = json.dumps(tallennettava)
         data.write(arvo)
